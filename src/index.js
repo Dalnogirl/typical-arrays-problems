c0e11102b9a43f965f83393329a9bf0f9e40b4b7
Hleb <velikiizevs@gmail.com>
2020-03-05 22:17:04 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0 | !Array.isArray(array)) {
+    return 0;
+  } else {
+    return array.sort((a, b) => a - b)[0];
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0 | !Array.isArray(array)) {
+    return 0;
+  } else {
+    return array.sort((a, b) => b - a)[0];
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0 | !Array.isArray(array)) {
+    return 0;
+  } else {
+    return array.reduce((acc, i) => acc += i) / array.length;
+  }
 }
